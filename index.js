fa2505ecf1e0b536296841fd3c33937aaf54187b
jeffmon <jeffmon@gmail.com>
2017-07-29 17:28:54 -1000

    finished open and close functions

--- index.js
+++ index.js
@@ -5,30 +5,54 @@ const five = require("johnny-five");
 const board = new five.Board();
 var led;
 var servo;
+var piezo;
+var door;
 
 board.on("ready", () => {
   led = new five.Led(7);
-
   servo = new five.Servo({
-    pin:14,
+    pin: 14,
     startAt: 75
   })
-   led.blink(500);
+  //
+  door = new five.Servo({
+    pin: 15,
+    startAt:75
+  })
+
+  piezo = new five.Piezo({
+    pin: 5
+  })
 })
+
+app.get("/treat", (req, res) => {
+  led.on();
+  servo.to(3);
+  setTimeout(() => {
+    servo.to(75);
+  }, 500)
+  res.end("treat!");
+});
+
+app.get("/sound", (req, res) => {
+  piezo.frequency(2400, 400);
+  led.blink(500);
+  res.end("sound!");
+})
+
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
 
 app.get("/open", (req, res) => {
   led.on();
-  servo.to(3);
+  door.to(175);
   res.end("open!");
 });
 
 app.get("/close", (req, res) => {
   led.on();
-  servo.to(75);
+  door.to(75);
   res.end("close!");
-});
-
-
-app.listen(port, () => {
-  console.log(`Listening on ${port}`);
 });
